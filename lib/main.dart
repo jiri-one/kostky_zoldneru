b7749239b32f0749598d49be2430dbe0bbfbdea5
jiri-one <nemec@jiri.one>
2025-06-30 14:58:23 +0200

    WIP, switched to async code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:dice_thrower/dice_thrower.dart' show diceThrower;
+import 'package:dice_thrower/dice_thrower.dart'
+    show diceThrower, oneDiceThrower;
 
 void main() {
   runApp(const MyApp());
@@ -8,27 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Kostky žoldnéřů'),
@@ -38,15 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -60,46 +36,39 @@ class _MyHomePageState extends State<MyHomePage> {
   int _sixDiceValue2 = 6;
   int _sixDiceValue3 = 6;
 
-  void getTwentyDiceValue() {
+  void getTwentyDiceValue() async {
+    int value = await oneDiceThrower(20);
     setState(() {
-      _twentyDiceValue = diceThrower(['20'])[0];
+      _twentyDiceValue = value;
     });
   }
 
-  void getSixDiceValue() {
+  void getSixDiceValue() async {
+    int value = await oneDiceThrower(6);
     setState(() {
-      _sixDiceValue = diceThrower(['6'])[0];
+      _sixDiceValue = value;
     });
   }
 
-  void getSixDiceValue2() {
+  void getSixDiceValue2() async {
+    int value = await oneDiceThrower(6);
     setState(() {
-      _sixDiceValue2 = diceThrower(['6'])[0];
+      _sixDiceValue2 = value;
     });
   }
 
-  void getSixDiceValue3() {
+  void getSixDiceValue3() async {
+    int value = await oneDiceThrower(6);
     setState(() {
-      _sixDiceValue3 = diceThrower(['6'])[0];
+      _sixDiceValue3 = value;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: LayoutBuilder(
